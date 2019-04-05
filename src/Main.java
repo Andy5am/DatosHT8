38fa0585df43ee4ad47bb8eeb80c061b5acc861a
Andy5am <38666859+Andy5am@users.noreply.github.com>
2019-04-05 11:18:18 -0600

    Menu opciones

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,7 @@ public class Main {
         ArrayList<String> archivo = new ArrayList<String>();
         try {
             Stream<String> lines = Files.lines(
-                    Paths.get(System.getProperty("user.dir")+"/pacientes.txt"),
+                    Paths.get(System.getProperty("user.dir")+"/src/pacientes.txt"),
                     StandardCharsets.UTF_8
             );
             lines.forEach(a -> archivo.add(a));
@@ -40,39 +40,84 @@ public class Main {
             Paciente paciente = new Paciente(pacientes[0],pacientes[1],pacientes[2]);
             lista.add(paciente);
         }
+        Scanner input = new Scanner(System.in);
 
-        //Se crea la PriorityQueue
-        PriorityQueue listaPacientes = new PriorityQueue(lista);
-        boolean continuar = true;
+        System.out.println("Que desea utilizar?");
+        System.out.println("1. VectorHeap");
+        System.out.println("2. PriorityQueue");
 
-        //El menu
-        while (continuar){
+        String opcion = input.nextLine();
+        if (opcion.equals("1")){
 
-            System.out.println("\nBienvenido a su lista de espera de pacientes");
-            System.out.println("Sus opciones son: ");
-            System.out.println("1. Siguiente paciente");
-            System.out.println("2. Salir");
-            System.out.println("Que desea hacer doctor?");
-            Scanner input = new Scanner(System.in);
-            String respuesta = input.nextLine();
+            VectorHeap listaPacientes = new VectorHeap(lista);
+            boolean continuar = true;
 
-            if (respuesta.equals("1")){
+            //El menu
+            while (continuar){
 
-                if (!listaPacientes.isEmpty()) {
-                    //Para que imprima los pacientes que necesiten de atencion de acuerdo a prioridad
-                    System.out.println(listaPacientes.remove());
-                }else {
-                    //Dice cuando ya no hayan mas pacientes
-                    System.out.println("No hay mas pacientes Doc");
+                System.out.println("\nBienvenido a su lista de espera de pacientes");
+                System.out.println("Sus opciones son: ");
+                System.out.println("1. Siguiente paciente");
+                System.out.println("2. Salir");
+                System.out.println("Que desea hacer doctor?");
+                String respuesta = input.nextLine();
+
+                if (respuesta.equals("1")){
+
+                    if (!listaPacientes.isEmpty()) {
+                        //Para que imprima los pacientes que necesiten de atencion de acuerdo a prioridad
+                        System.out.println(listaPacientes.remove());
+                    }else {
+                        //Dice cuando ya no hayan mas pacientes
+                        System.out.println("No hay mas pacientes Doc");
+                    }
+                    //opcion para que salga
+                } else if (respuesta.equals("2")) {
+                    continuar = false;
+                    System.out.println("Gran trabajo doc");
+                } else {
+                    //Por si no escribe una de las opciones habiles
+                    System.out.println("Opcion invalida");
                 }
-                //opcion para que salga
-            } else if (respuesta.equals("2")) {
-                continuar = false;
-                System.out.println("Gran trabajo doc");
-            } else {
-                //Por si no escribe una de las opciones habiles
-                System.out.println("Opcion invalida");
             }
+
+        }
+        else if (opcion.equals("2")) {
+
+            //Se crea la PriorityQueue
+            PriorityQueue listaPacientes = new PriorityQueue(lista);
+            boolean continuar = true;
+
+            //El menu
+            while (continuar) {
+
+                System.out.println("\nBienvenido a su lista de espera de pacientes");
+                System.out.println("Sus opciones son: ");
+                System.out.println("1. Siguiente paciente");
+                System.out.println("2. Salir");
+                System.out.println("Que desea hacer doctor?");
+                String respuesta = input.nextLine();
+
+                if (respuesta.equals("1")) {
+
+                    if (!listaPacientes.isEmpty()) {
+                        //Para que imprima los pacientes que necesiten de atencion de acuerdo a prioridad
+                        System.out.println(listaPacientes.remove());
+                    } else {
+                        //Dice cuando ya no hayan mas pacientes
+                        System.out.println("No hay mas pacientes Doc");
+                    }
+                    //opcion para que salga
+                } else if (respuesta.equals("2")) {
+                    continuar = false;
+                    System.out.println("Gran trabajo doc");
+                } else {
+                    //Por si no escribe una de las opciones habiles
+                    System.out.println("Opcion invalida");
+                }
+            }
+        }else {
+            System.out.println("Opcion invalida");
         }
     }
 }
